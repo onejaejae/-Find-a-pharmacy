4cf9ae091b680850389d4ecf706f2684bb4a5790
onejaejae <62149784+onejaejae@users.noreply.github.com>
2020-09-14 00:34:23 +0900

    Update MapCreate.js
    == => === 으로 변경, 데이터 없는거 모달창에 나오는 방식 변경

--- src/components/MapCreate.js
+++ src/components/MapCreate.js
@@ -20,7 +20,7 @@ class MapCreate extends React.Component {
       });
 
       
-      if (element.tel != null && element.tel.length == 7 && !(element.title == "백제약국") && (element.tel.indexOf("-") < 0))  { //1231234
+      if (element.tel != null && element.tel.length === 7 && !(element.title === "백제약국") && (element.tel.indexOf("-") < 0))  { //1231234
         try {
           if(element.adress != null){
             if((element.adress.indexOf("부천") > 0)){
@@ -41,13 +41,13 @@ class MapCreate extends React.Component {
           alert(err);
         }
       }
-      else if(element.tel != null && element.tel.length == 7 && element.title == "백제약국"){ //api 오류(031-533)
+      else if(element.tel != null && element.tel.length === 7 && element.title === "백제약국"){ //api 오류(031-533)
         element.tel = null;
       }
-      else if(element.tel != null && element.title == "열린온누리약국" && element.adress != null && element.adress.indexOf("화성")){ //api 오류
+      else if(element.tel != null && element.title === "열린온누리약국" && element.adress != null && element.adress.indexOf("화성")){ //api 오류
         element.tel = null;
       }
-      else if(element.tel != null && element.tel.length ==7 && (element.tel.indexOf("-") > 0)){
+      else if(element.tel != null && element.tel.length === 7 && (element.tel.indexOf("-") > 0)){
         try {
           if(element.adress != null){
             if((element.adress.indexOf("부천") > 0)){
@@ -65,7 +65,7 @@ class MapCreate extends React.Component {
           alert(err);
         }
       }
-      else if (element.tel != null && (element.tel.length == 8 || element.tel.length == 9) && !element.tel.startsWith("02")) {//123-1234, 12341234, 1234-1234
+      else if (element.tel != null && (element.tel.length === 8 || element.tel.length === 9) && !element.tel.startsWith("02")) {//123-1234, 12341234, 1234-1234
         try {
           if(element.adress != null){
             if((element.adress.indexOf("부천") > 0)){
@@ -84,7 +84,7 @@ class MapCreate extends React.Component {
           alert(err);
         }
       }
-      else if (element.tel != null && element.tel.length == 9 && element.tel.startsWith("02")) {
+      else if (element.tel != null && element.tel.length === 9 && element.tel.startsWith("02")) {
         try {
           if (element.adress != null) {
             element.tel = element.tel.slice(0, 2) + "-" + element.tel.slice(2, 5) + "-" + element.tel.slice(5);
@@ -94,7 +94,7 @@ class MapCreate extends React.Component {
           alert(element.tel);
         }
       }
-      else if (element.tel != null && element.tel.length == 10 && !element.tel.startsWith("02")) {
+      else if (element.tel != null && element.tel.length === 10 && !element.tel.startsWith("02")) {
         try {
           if (element.adress != null) {
             element.tel = element.tel.slice(0, 3) + "-" + element.tel.slice(3, 6) + "-" + element.tel.slice(6);
@@ -104,7 +104,7 @@ class MapCreate extends React.Component {
           alert(element.tel);
         }
       }
-      else if (element.tel != null && element.tel.length == 10 && element.tel.startsWith("02")) {
+      else if (element.tel != null && element.tel.length === 10 && element.tel.startsWith("02")) {
         try {
           if (element.adress != null) {
             element.tel = element.tel.slice(0, 2) + "-" + element.tel.slice(2, 6) + "-" + element.tel.slice(6);
@@ -113,8 +113,8 @@ class MapCreate extends React.Component {
         catch (err) {
           alert(element.tel);
         }
-      }else if(element.tel != null && element.tel == "031-863-696"){}
-      else if (element.tel != null && element.tel.length == 11 && element.tel.startsWith("031") && (element.tel.indexOf("-") > 0)) {
+      }else if(element.tel != null && element.tel === "031-863-696"){}
+      else if (element.tel != null && element.tel.length === 11 && element.tel.startsWith("031") && (element.tel.indexOf("-") > 0)) {
         try {   //031123-1234
           if (element.adress != null) {
             element.tel = element.tel.slice(0, 3) + "-" + element.tel.slice(3);
@@ -123,10 +123,10 @@ class MapCreate extends React.Component {
         catch (err) {
           alert(element.tel);
         }
-      }else if (element.tel != null && element.tel.length == 12 && element.tel.startsWith("031-")) {
+      }else if (element.tel != null && element.tel.length ===12 && element.tel.startsWith("031-")) {
         //031-123-1234 (12)
       }
-      else if (element.tel != null && element.tel.length == 12 && element.tel.startsWith("031")) {
+      else if (element.tel != null && element.tel.length === 12 && element.tel.startsWith("031")) {
         try {   //0311234-1234
           if (element.adress != null) {
             element.tel = element.tel.slice(0, 3) + "-" + element.tel.slice(3);
@@ -136,12 +136,12 @@ class MapCreate extends React.Component {
           alert(element.tel);
         }
       }
-      else if(element.tel!= null && (element.tel.indexOf("-") < 0) && element.tel.length == 11){ //03112341234
+      else if(element.tel!= null && (element.tel.indexOf("-") < 0) && element.tel.length === 11){ //03112341234
         element.tel = element.tel.slice(0, 3) + "-" + element.tel.slice(3, 7) + "-" + element.tel.slice(7);
       }
       
       
-      console.log(element.tel +"  "+ element.title);
+  
 
       
 
@@ -202,7 +202,7 @@ class MapCreate extends React.Component {
       modal_addr1.className = "modal_addr1";
       doro_container.appendChild(modal_addr1);
 
-      modal_addr1.innerHTML = `${element.roadAdress ? element.roadAdress : "-"}`;
+      modal_addr1.innerHTML = `${element.roadAdress ? element.roadAdress : "도로명 주소 정보가 없습니다"}`;
 
       var jibeon_container = document.createElement('div');
       jibeon_container.className = "jibeon_container";
@@ -218,7 +218,7 @@ class MapCreate extends React.Component {
       modal_addr2.className = "modal_addr2";
       jibeon_container.appendChild(modal_addr2);
 
-      modal_addr2.innerHTML = `${element.adress ? element.adress : "-"}`;
+      modal_addr2.innerHTML = `${element.adress ? element.adress : "주소 정보가 없습니다"}`;
 
       var modal_ph_container = document.createElement('div');
       modal_ph_container.className = 'modal_ph_container';
@@ -234,7 +234,7 @@ class MapCreate extends React.Component {
       num1.className = "num1";
       modal_ph_container.appendChild(num1);
 
-      num1.innerHTML = `${element.tel ? element.tel : "-"}`;
+      num1.innerHTML = `${element.tel ? element.tel : "연락처 정보가 없습니다"}`;
 
       var modal_footer = document.createElement('div');
       modal_footer.className = "modal_footer";
@@ -253,7 +253,7 @@ class MapCreate extends React.Component {
       var modal_p_inner = document.createElement('span');
       modal_p_inner.className = "modal_p_inner";
       modal_p.appendChild(modal_p_inner);
-      modal_p_inner.innerHTML = "전화로 약국 별 취급 의약품을 확인 후 방문하시길 바랍니다.";
+      modal_p_inner.innerHTML = "전화로 약국 별 취급 의약품을 확인 후 방문하시길 바랍니다";
 
 
 
@@ -280,8 +280,7 @@ class MapCreate extends React.Component {
 
   mapScript = () => {
     const { AnimalPharmacyData } = this.props;
-    console.log(AnimalPharmacyData[1].lat, AnimalPharmacyData[1].lng);
-
+  
     let container = document.getElementById("Mymap");
 
     let option = {
